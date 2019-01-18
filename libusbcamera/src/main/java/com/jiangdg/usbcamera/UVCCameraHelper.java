5b0aa8c874fbc4616930390352eb595871a28775
孙攀 <sunpan920@gmail.com>
2019-01-18 16:44:11 +0800

    问题修复

--- libusbcamera/src/main/java/com/jiangdg/usbcamera/UVCCameraHelper.java
+++ libusbcamera/src/main/java/com/jiangdg/usbcamera/UVCCameraHelper.java
@@ -117,18 +117,15 @@ public class UVCCameraHelper {
             public void onConnect(final UsbDevice device, USBMonitor.UsbControlBlock ctrlBlock, boolean createNew) {
                 mCtrlBlock = ctrlBlock;
                 openCamera(ctrlBlock);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // 休眠500ms，等待Camera创建完毕
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        // 开启预览
-                        startPreview(mCamView);
+                new Thread(() -> {
+                    // 休眠500ms，等待Camera创建完毕
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
+                    // 开启预览
+                    startPreview(mCamView);
                 }).start();
                 if (listener != null) {
                     listener.onConnectDev(device, true);
